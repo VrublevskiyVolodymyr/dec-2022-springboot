1861de4c5d39f1700c4bad61e76f673615b7352d
VrublevskiyVolodymyr <115405839+VrublevskiyVolodymyr@users.noreply.github.com>
2023-07-11 23:16:10 +0300

    Initial commit

---
+++ src/main/java/ua/com/owu/dec2022springboot/Dec2022SpringbootApplication.java
@@ -0,0 +1,13 @@
+package ua.com.owu.dec2022springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dec2022SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dec2022SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/owu/dec2022springboot/Dec2022SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.owu.dec2022springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dec2022SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
