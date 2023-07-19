4aedde8ab461810b9e264d39a72fbec523bd7eec
VrublevskiyVolodymyr <115405839+VrublevskiyVolodymyr@users.noreply.github.com>
2023-07-19 22:28:24 +0300

    del import

--- src/main/java/ua/com/owu/dec2022springboot/services/MailService.java
+++ src/main/java/ua/com/owu/dec2022springboot/services/MailService.java
@@ -1,15 +1,19 @@
 package ua.com.owu.dec2022springboot.services;
 
-import jakarta.mail.MessagingException;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -20,13 +24,19 @@ public class MailService {
     private JavaMailSender javaMailSender;
 
     @SneakyThrows
-    public void sendEmail(String email, String body) {
+    public void sendEmail(String email, String body, Optional<File> file) {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
 
-            helper.setTo(email);
-            helper.setText("<h2> " + body + "</h2>", true);
-            helper.setFrom(new InternetAddress("v637904@gmail.com"));
+        helper.setTo(email);
+        helper.setText("<h2> " + body + "</h2>", true);
+
+        if (file.isPresent()) {
+            File attachment = file.get();
+            FileSystemResource fileSystemResource = new FileSystemResource(attachment);
+            helper.addAttachment(attachment.getName(), fileSystemResource);
+        }
+        helper.setFrom(new InternetAddress("v637904@gmail.com"));
         javaMailSender.send(mimeMessage);
     }
 }
